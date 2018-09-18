0b76770aefcbf72cbc2c2bb0c17afff71053884b
lee-sj <201302386@hufs.ac.kr>
2018-09-18 07:39:52 +0000

    create model Profile

---
+++ db/migrate/20180918073806_create_profiles.rb
@@ -0,0 +1,13 @@
+class CreateProfiles < ActiveRecord::Migration[5.0]
+  def change
+    create_table :profiles do |t|
+      t.string :name
+      t.string :mobile
+      t.string :address
+      t.date :birth
+      t.belongs_to :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
