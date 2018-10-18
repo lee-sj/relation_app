7759d8be877e7eb28298e04a97be8df460a69ce0
lee-sj <201302386@hufs.ac.kr>
2018-10-18 06:49:07 +0000

    comment view&controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,24 @@
 class CommentsController < ApplicationController
+    before_action :set_comment, only: [:destroy]
     def create 
-        
+        @comment = Comment.new(comment_params) 
+        @comment.user = current_user
+        @comment.save 
+        redirect_to @comment.commentable
     end 
     
     def destroy 
-        
+        @comment.destroy
+        redirect_to @comment.commentable
     end
+    
+    private
+    def set_comment
+        @comment = Comment.find params[:id]    
+    end 
+    
+    def comment_params
+        params.require(:comment).permit(:content, :commentable_id, :commentable_type )
+    end 
+    
 end
